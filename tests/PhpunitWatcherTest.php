65f2d7df9ff81c3b70910f303f213c87bb90f377
freek <freek@spatie.be>
2017-07-26 16:27:32 -0400

    add test

---
+++ tests/PhpunitWatcherTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Spatie\Skeleton\Test;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Process\Process;
+
+class PhpunitWatcherTest extends TestCase
+{
+    /** @test */
+    public function the_watcher_can_be_executed()
+    {
+        $process = new Process('./phpunit-watcher');
+
+        $process->run();
+
+        $this->assertTrue($process->isSuccessful());
+    }
+}
