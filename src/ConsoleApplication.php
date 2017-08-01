831d6d377c335909ff2d27ea280625590e5be0b6
Freek Van der Herten <freek@spatie.be>
2017-08-01 14:38:03 +0200

    Update ConsoleApplication.php

--- src/ConsoleApplication.php
+++ src/ConsoleApplication.php
@@ -8,7 +8,7 @@ class ConsoleApplication extends Application
 {
     public function __construct()
     {
-        parent::__construct('PHPUnit Watcher', '1.0.1');
+        parent::__construct('PHPUnit Watcher', '1.0.4');
 
         $this->add(new WatcherCommand());
     }
