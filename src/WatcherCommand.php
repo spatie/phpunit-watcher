1e3cc43aa0a2895dec7a700439a8f637ae7f6ec5
Freek Van der Herten <freek@spatie.be>
2017-08-05 12:59:02 +0200

    Apply fixes from StyleCI (#25)

--- src/WatcherCommand.php
+++ src/WatcherCommand.php
@@ -2,13 +2,13 @@
 
 namespace Spatie\PhpUnitWatcher;
 
-use Spatie\PhpUnitWatcher\Exceptions\InvalidConfigfile;
 use Symfony\Component\Yaml\Yaml;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Spatie\PhpUnitWatcher\Exceptions\InvalidConfigfile;
 
 class WatcherCommand extends Command
 {
