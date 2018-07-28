f99cea900fbdf33b2e07f9d6972729a82a74082b
Freek Van der Herten <freek@spatie.be>
2018-07-27 20:26:18 -0500

    Apply fixes from StyleCI (#69)

--- src/Screens/Completers/FilenameCompleter.php
+++ src/Screens/Completers/FilenameCompleter.php
@@ -5,16 +5,17 @@ namespace Spatie\PhpUnitWatcher\Screens\Completers;
 class FilenameCompleter extends Completer
 {
     /**
-     * Search for files and directories corresponding to the word to complete
+     * Search for files and directories corresponding to the word to complete.
      *
      * @return array
      */
-    protected function search() {
-        return glob($this->word . '*', GLOB_MARK) ?: [];
+    protected function search()
+    {
+        return glob($this->word.'*', GLOB_MARK) ?: [];
     }
 
     /**
-     * Append a quote if path is prefixed with a quote, except for directories
+     * Append a quote if path is prefixed with a quote, except for directories.
      *
      * @param string $path
      * @return string
@@ -28,7 +29,7 @@ class FilenameCompleter extends Completer
         $char = $this->getCharBeforeWord();
 
         return ($char === '"' || $char === '\'')
-            ? $path . $char
+            ? $path.$char
             : $path;
     }
 }
