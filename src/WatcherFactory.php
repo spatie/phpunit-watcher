1fc964184095fff16e28276fe118d25e801d1232
Ian Dunn <ian@iandunn.name>
2020-10-20 23:55:38 -0700

    WatcherFactory: Merge options recursively to preserve nested values. (#115)
    * WatcherFactory: Merge options recursively to preserve nested values.
    
    Previously, the following configuration would result in a file mask of `null`, because the entire `watch` item would be overridden.
    
    ```yml
    watch:
      directories:
        - ./public_html/wp-content/mu-plugins/
        - ./public_html/wp-content/plugins/camptix/
        - ./public_html/wp-content/plugins/wordcamp-organizer-reminders/
        - ./public_html/wp-content/plugins/wordcamp-remote-css/
        - ./public_html/wp-content/plugins/wordcamp-speaker-feedback/
    
    notifications:
      passingTests: false
      failingTests: false
    ```
    
    Now users can selective set a nested option like `watch.fileMask`, without having to set everything inside `watch`.
    
    * get tests working
    
    * exempt directories from merge

--- src/WatcherFactory.php
+++ src/WatcherFactory.php
@@ -30,10 +30,9 @@ class WatcherFactory
         return [$watcher, $options];
     }
 
-    protected static function mergeWithDefaultOptions(array $options): array
+    public static function getDefaultOptions(): array
     {
-        $options = array_merge([
-
+        return [
             'watch' => [
                 'directories' => [
                     'app',
@@ -47,16 +46,29 @@ class WatcherFactory
                 'failingTests' => true,
             ],
             'hideManual' => false,
-        ], $options);
+        ];
+    }
 
-        $options['watch']['directories'] = array_map(function ($directory) {
+    protected static function mergeWithDefaultOptions(array $userOptions): array
+    {
+        // Merge all options with the defaults, so that there's always a complete set.
+        $mergedOptions = array_replace_recursive(self::getDefaultOptions(), $userOptions);
+
+        // Exception to above: Allow directories to be overwritten entirely, because that's usually desired.
+        if (isset($userOptions['watch']['directories'])) {
+            $mergedOptions['watch']['directories'] = $userOptions['watch']['directories'];
+        }
+
+        $mergedOptions['watch']['directories'] = array_unique($mergedOptions['watch']['directories']);
+
+        $mergedOptions['watch']['directories'] = array_map(function ($directory) {
             return getcwd()."/{$directory}";
-        }, $options['watch']['directories']);
+        }, $mergedOptions['watch']['directories']);
 
-        $options['watch']['directories'] = array_filter($options['watch']['directories'], function ($directory) {
+        $mergedOptions['watch']['directories'] = array_filter($mergedOptions['watch']['directories'], function ($directory) {
             return file_exists($directory);
         });
 
-        return $options;
+        return $mergedOptions;
     }
 }
---
+++ tests/WatcherFactoryTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Spatie\PhpUnitWatcher\Test;
+
+use PHPUnit\Framework\TestCase;
+use Spatie\PhpUnitWatcher\WatcherFactory;
+
+class WatcherFactorTest extends TestCase
+{
+    /** @test */
+    public function it_can_be_instantiated()
+    {
+        $factory = new WatcherFactory();
+
+        $this->assertInstanceOf(WatcherFactory::class, $factory);
+    }
+
+    /** @test */
+    public function setting_notification_preserves_other_options()
+    {
+        $userOptions = [
+            'notifications' => [
+                'passingTests' => false,
+            ],
+        ];
+
+        $actualOptions = WatcherFactory::create($userOptions)[1];
+
+        $this->assertFalse($actualOptions['notifications']['passingTests']);
+        $this->assertTrue($actualOptions['notifications']['failingTests']);
+        $this->assertFalse($actualOptions['hideManual']);
+        $this->assertSame('*.php', $actualOptions['watch']['fileMask']);
+    }
+
+    /** @test */
+    public function setting_directories_preserves_other_options()
+    {
+        $userOptions = [
+            'watch' => [
+                'directories' => ['lib', 'tests'],
+            ],
+        ];
+
+        $actualOptions = WatcherFactory::create($userOptions)[1];
+
+        $this->assertTrue($actualOptions['notifications']['failingTests']);
+        $this->assertFalse($actualOptions['hideManual']);
+        $this->assertSame('*.php', $actualOptions['watch']['fileMask']);
+    }
+}
