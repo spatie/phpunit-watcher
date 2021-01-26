1aa289498d0360213b9fb42b3875d2372f4e5edb
Ian Dunn <ian@iandunn.name>
2021-01-26 21:18:23 +0000

    minor cleanup

--- src/WatcherCommand.php
+++ src/WatcherCommand.php
@@ -6,7 +6,6 @@ use Spatie\PhpUnitWatcher\Exceptions\InvalidConfigfile;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Yaml\Yaml;
